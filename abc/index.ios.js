9ec935c430c37db741f2f39786c4187906b3378b
chaitanyaparashar <cparashar007@gmail.com>
2017-07-26 10:45:43 +0530

    Revert "Login using firebase"
    This reverts commit ddaf888389cdd016d2760d60b4b7130e786ddeec.

--- abc/index.ios.js
+++
@@ -1,7 +0,0 @@
-//import a library to help create a omponent
-import { AppRegistry} from 'react-native';
-import App from './src/app';
-//Create a Component
-
-//render it to the device
-AppRegistry.registerComponent('abc',() => App);
